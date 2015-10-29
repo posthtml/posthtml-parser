43ab5da2c5946dd3202f93722e68fbbe02fca198
Ivan Voischev <voischev.ivan@ya.ru>
2015-10-29 08:36:33 +0300

    fix test texts & add test tag

--- test/test.js
+++ test/test.js
@@ -12,15 +12,29 @@ describe('PostHTML-Parser test', function() {
         expect(parser(html)).to.eql(tree);
     });
 
-    it('comment', function() {
-        expect(parser('<div><!--comment--></div>')).to.eql([{tag: 'div', content: ['<!--comment-->']}]);
-    });
-
-    it('last comment', function() {
+    it('should be parse comment', function() {
         expect(parser('<!--comment-->')).to.eql(['<!--comment-->']);
     });
 
-    it('doctype', function() {
+    it('should be parse comment in content', function() {
+        expect(parser('<div><!--comment--></div>')).to.eql([{tag: 'div', content: ['<!--comment-->']}]);
+    });
+
+    it('should be parse doctype', function() {
+        expect(parser('<!doctype html>')).to.eql(['<!doctype html>']);
+    });
+
+    it('should be parse tag', function() {
+        expect(parser('<html></html>')).to.eql([{ tag: 'html' }]);
+    });
+
+    it('should be parse doctype and tag', function() {
         expect(parser('<!doctype html><html></html>')).to.eql(['<!doctype html>', { tag: 'html' }]);
+    });
+
+    it('should be parse tag attrs', function() {
+        expect(parser('<div id="id" class="class"></div>')).to.eql([{
+            tag: 'div', attrs: { id: 'id', class: 'class'} }
+        ]);
     });
 });
