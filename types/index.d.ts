eb6394081186809955e32e0fc66ba953b5601297
Ivan Demidov <Scrum@list.ru>
2021-02-05 00:18:07 +0300

    perf: export default declare

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,9 @@
 import {ParserOptions} from 'htmlparser2';
 
+declare const parser: (html: string, options?: Options) => Node[];
+
+export default parser;
+
 export type Directive = {
   name: string | RegExp;
   start: string;
